a53fe75a5b211dedff0e13987066eb3dab25d3e4
ludelvs <vgns92ps@gmail.com>
2018-05-07 00:48:33 +0900

    update start-tmux.sh
    - 追加セッションでログが作成されなかったのを修正

--- bin/start-tmux.sh
+++ bin/start-tmux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-[ ! $TMUX ] && tmux
-[ $TMUX ] && tmux pipe-pane -o '/bin/sh -c "~/bin/tmux_logging.sh ~/.tmux/log #S #I #P local"'
+[ ! $TMUX ] && touch /var/tmp/start-tmux && tmux
+if [ $TMUX -a -f "/var/tmp/start-tmux" ]; then
+  tmux pipe-pane -o '/bin/sh -c "~/bin/tmux_logging.sh ~/.tmux/log #S #I #P local"'
+  rm /var/tmp/start-tmux
+fi
